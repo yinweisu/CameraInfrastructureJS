04458451b02f3be2331f02446bcb43151fa5bb84
yinweisu <wyin@ucdavis.edu>
2021-06-03 12:38:42 -0700

    pusher

--- frontend/pusher.js
+++ frontend/pusher.js
@@ -5,15 +5,31 @@ const room_id = '123';
 let socket = io(SIGNALING_SERVER_URL, { autoConnect: true });
 var peer_connection = null;
 
-let remote_stream_element = document.querySelector('remote_stream');
+var media_constraints = {
+    video: true,
+};
+
+let local_stream_element = document.querySelector('local_stream');
 
 function create_peer_connection() {
     if (peer_connection == null) {
         peer_connection = new RTCPeerConnection(PC_CONFIG);
+        peer_connection.onnegotiationneeded = handle_negotiation;
         peer_connection.onicecandidate = handle_icecandidate;
-        peer_connection.ontrack = handle_ontrack;
         peer_connection.oniceconnectionstatechange = handle_ice_connection_state_change;
     }
+}
+
+function handle_negotiation() {
+    peer_connection.createOffer().then(function(offer) {
+        return peer_connection.setLocalDescription(offer);
+    }).then(function() {
+        const data = {
+            type: 'push_offer',
+            sdp: peer_connection.localDescription
+        };
+        socket.emit('data', data);
+    })
 }
 
 function handle_icecandidate(event) {
@@ -27,11 +43,6 @@ function handle_icecandidate(event) {
     }
 }
 
-function handle_ontrack(event) {
-    console.log('Add track');
-    remote_stream_element.srcObject = event.streams[0];
-}
-
 function handle_ice_connection_state_change(event) {
     switch (peer_connection.iceConnectionState) {
         case 'closed':
@@ -41,24 +52,9 @@ function handle_ice_connection_state_change(event) {
     }
 }
 
-function handle_push_offer(sdp){
-    if (peer_connection == null) {
-        create_peer_connection();
-    }
-    var remote_stream = null;
-
+function handle_push_answer(sdp){
     var remote_description = new RTCSessionDescription(sdp);
-    peer_connection.setRemoteDescription(remote_description).then(function() {
-        return peer_connection.createAnswer();
-    }).then(function() {
-        return peer_connection.setLocalDescription(answer);
-    }).then(function() {
-        const data = {
-            type: 'push_answer',
-            sdp: peer_connection.localDescription
-        };
-        socket.emit('data', data);
-    })
+    await peer_connection.setRemoteDescription(remote_description);
 }
 
 function handle_new_ice_candidate(sdp) {
@@ -67,29 +63,52 @@ function handle_new_ice_candidate(sdp) {
 
 function stop() {
     if (peer_connection) {
+        peer_connection.onnegotiationneeded = null;
         peer_connection.onicecandidate = null;
-        peer_connection.onaddstream = null;
         peer_connection.oniceconnectionstatechange = null;
 
-        if (remote_stream_element.srcObject) {
-            remote_stream_element.srcObject.getTracks().foreach(track => track.stop());
+        if (local_stream_element.srcObject) {
+            local_stream_element.srcObject.getTracks().foreach(track => track.stop());
         }
 
         peer_connection.close();
         peer_connection = null;
     }
-    remote_stream_element.removeAttribute('srcObject');
+    local_stream_element.removeAttribute('srcObject');
+}
+
+function handle_get_user_media_error(e) {
+    switch(e.name) {
+        case 'NotFoundError':
+            alert('Unable to push video because no camera was found');
+            break;
+        case 'SecurityError':
+        case 'PermissionDeniedError':
+            break;
+        default:
+            alert('Error opening your camera ' + e.message);
+            break;
+    }
+    stop();
 }
 
 socket.on('connected', () => {
     const data = {
-        'role': 'viewer', 'room_id': '123'
+        'role': 'pusher', 'room_id': '123'
     };
     socket.emit('join', data);
 });
 
 socket.on('ready', () => {
     create_peer_connection();
+
+    navigator.mediaDevices.getUserMedia(media_constraints)
+    .then(function(local_stream) {
+        local_stream_element.srcObject = local_stream;
+        local_stream.getTracks().forEach(track => peer_connection.addTrack(track, local_stream));
+    })
+    .catch(handle_get_user_media_error);
+
 });
 
 socket.on('peer_left', (data) => {
@@ -100,8 +119,8 @@ socket.on('data', (data) => {
     const type = data.type;
     const sdp = data.sdp;
     switch (type) {
-        case 'push_offer':
-            hanle_push_offer(sdp);
+        case 'push_answer':
+            hanle_push_answer(sdp);
             break;
         case 'new_ice_candidate':
             handle_new_ice_candidate(sdp);
